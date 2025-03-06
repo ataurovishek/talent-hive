78d1045336c9b36efad1c69689a97bb7d07854c7
ataur rahman <ataurovishek@gmail.com>
2025-03-07 01:40:54 +0600

    new solved

--- components/general/UploadThingReexported.jsx
+++ components/general/UploadThingReexported.jsx
@@ -1,10 +1,9 @@
-
 import {
     generateUploadButton,
     generateUploadDropzone,
 } from "@uploadthing/react";
 
+import { ourFileRouter } from "@/app/api/uploadthing/core";
 
-
-export const UploadButton = generateUploadButton();
-export const UploadDropzone = generateUploadDropzone();
+export const UploadButton = generateUploadButton(ourFileRouter);
+export const UploadDropzone = generateUploadDropzone(ourFileRouter);
