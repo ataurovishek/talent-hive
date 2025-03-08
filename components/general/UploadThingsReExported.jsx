89492817ef5f032b2532686622c09ce4db88c935
ataur rahman <ataurovishek@gmail.com>
2025-03-09 02:11:59 +0600

    another change

--- components/general/UploadThingsReExported.jsx
+++
@@ -1,6 +0,0 @@
-import { generateUploadButton, generateUploadDropzone } from "@uploadthing/react";
-
-// ...existing code...
-
-export const UploadButton = generateUploadButton();
-export const UploadDropzone = generateUploadDropzone();
